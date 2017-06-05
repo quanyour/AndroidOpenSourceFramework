0995dd5240c1456902967ea848342d43028aff1c
yourui.quan <qzctxwd@qq.com>
2017-06-05 14:24:41 +0800

    readme.md

--- app/src/main/java/open/main/base/BaseActivity.java
+++ app/src/main/java/open/main/base/BaseActivity.java
@@ -1,5 +1,7 @@
 package open.main.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -10,6 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity{
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-
+    }
 }
